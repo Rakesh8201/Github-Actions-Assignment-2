1710eabddfd525a22e78a85f7a8f2799c5a6b73f
Rakesh8201 <rakeshchenna8201@gmail.com>
2024-03-01 16:25:09 +0530

    sample function added

--- Sample.java
+++ Sample.java
@@ -1,5 +1,16 @@
 public class Sample {
+
+    // sample function added
+    public static void sampleFunction(){
+        System.out.println(
+            "Iam Sample Function"
+        );
+    }
     public static void main(String Args[]){
-        System.out.println("Iam main function");
+
+        System.out.println(
+            "Iam main function"
+        );
+        Sample.sampleFunction();
     }
 }
